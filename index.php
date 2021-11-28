e98ccf9b178c41a87feb1cd7e6938590bcf956b7
Eleazar Ramos Cortés <sixen25@gmail.com>
2021-11-28 13:23:50 +0100

    first commit

--- index.php
+++ index.php
@@ -8,75 +8,182 @@
     <title>Agenda</title>
 </head>
 <body>
-    
-
 <h1>CONTACTOS</h1>
 <?php
 // INICIAMOS LA SESIÓN
+// Conexión a la base de datos...
+include_once "base_de_datos.php";
+
+//Inicio de sesiones de el usuario
 session_start();
-// Inicializamos las variables de sesión
-if (!isset($_SESSION["pagina"])) {  
-    $_SESSION["pagina"] = 1;
-} 
-// Control movimiento usuario
-if(!empty($_GET["pag"])){
-    if($_GET["pag"] == "atras"){
-        $_SESSION['pagina'] =  $_SESSION['pagina'] - 1 ;
-    } else if($_GET["pag"] == "adelante"){
-        $_SESSION['pagina'] =  $_SESSION['pagina'] + 1;
-    }   
-} else if(!empty($_GET["pagina"])){   
-    $_SESSION['pagina'] = $_GET["pagina"];
-}
 
 $productosPorPagina = 5;
 
-//MODIFICAR
-$paginas = 3;
+$limit = $productosPorPagina;
+
+// Necesitamos saber cuantas páginas vamos a mostrar
+$sentencia = $base_de_datos->query("SELECT count(*) AS n_contactos FROM contactes");
+$conteo = $sentencia->fetchObject()->n_contactos;
+
+$paginas = ceil($conteo / $productosPorPagina);
+
+
+
+
+
+// Inicializamos las variables de sesión
+if (!isset($_SESSION["pagina"])) {
+    $_SESSION["pagina"] = 1;
+}
+// Control movimiento usuario
+if (!empty($_GET["pag"])) {
+    if ($_GET["pag"] == "atras") {
+
+        if ($_SESSION['pagina'] - 1 == 0) { // prevenimos que no vaya paginas inexistentes.
+            $_SESSION['pagina'] = 1;
+        } else {
+            $_SESSION['pagina'] = $_SESSION['pagina'] - 1;
+        }
+
+    } else if ($_GET["pag"] == "adelante") {
+
+        if ($_SESSION['pagina'] + 1 > $paginas) { // prevenimos que no vaya paginas inexistentes.
+            $_SESSION['pagina'] = $paginas;
+        } else {
+            $_SESSION['pagina'] = $_SESSION['pagina'] + 1;
+        }
+    }
+} else if (!empty($_GET["pagina"])) {
+    $_SESSION['pagina'] = $_GET["pagina"];
+}
+
+$orderBy = 'id';
+// Sesion para cognoms
+if (!isset($_SESSION["cognoms"])) {
+    $_SESSION["cognoms"] = "ASC";
+    $cognoms = "DESC";
+} else {
+    $cognoms = "ASC";
+}
+
+if (isset($_GET["cognoms"])) {
+    $orderBy = 'cognoms';
+    $_SESSION["cognoms"] = $_GET["cognoms"]; // COMO SE TENDRÁ QUE MOSTRAR EN LA QUERY
+    $cognoms = $_SESSION["cognoms"];
+    $orderType = $cognoms;
+    // Preparo enlace de cognoms para que sea al revel cual vuelvan a pulsar
+    if ($cognoms == "DESC") {
+        $cognoms = "ASC";
+        $orderType = $cognoms;
+    } else {
+        $cognoms = "DESC";
+        $orderType = $cognoms;
+    }
+}
+
+// Sesion para id
+if (!isset($_SESSION["id"])) {
+    $_SESSION["id"] = "ASC";
+    $id = "DESC";
+} else {
+    $id = "ASC";
+}
+
+if (isset($_GET["id"])) {
+    $orderBy = 'id';
+    $_SESSION["id"] = $_GET["id"]; // COMO SE TENDRÁ QUE MOSTRAR EN LA QUERY
+    $id = $_SESSION["id"];
+    $orderType = $id;
+    // Preparo enlace de id para que sea al revel cual vuelvan a pulsar
+    if ($id == "DESC") {
+        $id = "ASC";
+        $orderType = $id;
+    } else {
+        $id = "DESC";
+        $orderType = $id;
+    }
+}
+
+if(empty($orderType)) {
+    $orderType = "ASC";
+}
+
+$orderbyAndType = $orderBy . " " . $orderType;
+
+
+// El offset para que saltemos X productos que viene dado por:
+// multiplicar (la página - 1 )* los productos por página
+$offset = ($_SESSION['pagina'] - 1) * $productosPorPagina;
+
+
+# Ahora obtenemos los productos usando ya el OFFSET y el LIMIT
+$sentencia = $base_de_datos->prepare("SELECT * FROM contactes ORDER BY " . $orderbyAndType  ." LIMIT ? OFFSET ?");
+$sentencia->execute([$limit, $offset]);
+$contactes = $sentencia->fetchAll(PDO::FETCH_OBJ);
+
+
 
 
 // Mostramos la información
 echo "<table>";
 echo '<thead>';
 echo '<tr>';
-echo '<th>Id</th>'; // <a href="./index.php?Id=', urlencode($Id), '&pagina=',urlencode($pagina),'"> Ordenar por ID ' . $Id . "</a>
-echo '<th>Cornom</th>'; // '<a href="./index.php?Cognom=', urlencode($Cognom), '&pagina=',urlencode($pagina),'"> Ordenar por Cognom ' . $Cognom. "</a>";
+echo '<th><a href="./index.php?id=', urlencode($id), '&pagina=', urlencode($_SESSION['pagina']), '"> Ordenar por ID ' . $id . "</a></th>";
+echo '<th><a href="./index.php?cognoms=', urlencode($cognoms), '&pagina=', urlencode($_SESSION['pagina']), '"> Ordenar por Cognom ' . $cognoms . "</a></th>";
 echo '<th>Nom</th>';
 echo '<th colspan="3">Manteniment</th>';
 echo '</tr>';
 echo '</thead>';
 echo '<tbody>';
-echo '<p class="infoPagina"> Página '.$_SESSION["pagina"].' de x </p>';
+echo '<p class="infoPagina"> Página ' . $_SESSION["pagina"] . ' de ' . $paginas . ' </p>';
 // MOSTRAMOS LA INFORMACIÓN
-echo "<div class='información'>";    
-for ($i=0; $i < 5; $i++) { 
+echo "<div class='información'>";
 
+
+echo "<div class='información'>";    
+foreach ($contactes as $contacte) { 
     echo '<tr>';
-    echo "<td>" . "id". $i ."</td>";
+    echo "<td>" . $contacte->id ."</td>";
     //echo " ";
-    echo "<td class='left'>" .  "cognom". $i ."</td>";
+    echo "<td>" . $contacte->cognoms ."</td>";
     //echo " ";
-    echo "<td class='left'>" .  "nom". $i ."</td>";
+    echo "<td>" . $contacte->nom ."</td>";
     //echo "<br>";
-    echo "<td><a href='./view.php?view=". $i ."1'><img src='./assets/img/view.png' alt='inicio' /></td></a>";
+    echo "<td><a href='./view.php?view=" . $contacte->id . "'><img src='./assets/img/view.png' alt='inicio' /></td></a>";
     //echo "<br>";
-    echo "<td><a href='./edit.php?edit=". $i ."'><img src='./assets/img/edit.png' alt='inicio' /></td></a>";
+    echo "<td><a href='./edit.php?edit=" . $contacte->id . "'><img src='./assets/img/edit.png' alt='inicio' /></td></a>";
     //echo "<br>";
-    echo "<td><a href='./remove.php?remove=". $i ."'><img src='./assets/img/remove.png' alt='inicio' /></td></a>";
+    echo "<td><a href='./remove.php?remove=" . $contacte->id . "'><img src='./assets/img/remove.png' alt='inicio' /></td></a>";
     //echo "<br>";
     echo '</tr>';
-    
 }
-echo '<tr>'
-    . '<td class="menuMovimiento" colspan="3">'
-    . '<a href="./index.php?pagina=1"><img src="./assets/img/home.png" alt="inicio"/></a>'
-    . '<a href="./index.php?pag=atras"><img src="./assets/img/left.png" alt="atrás"/></a>'
-    . '<a href="./index.php?pag=adelante"><img src="./assets/img/right.png" alt="adelante"/></a>'
-    . '<a href="./index.php?pagina='.$paginas.'"><img src="./assets/img/end.png" alt="final"/></a>'
-    . '</td>';
 
+echo '<tr>';
+if (isset($_GET["cognoms"])) { //$cognoms
+    echo '<td class="menuMovimiento" colspan="3">'
+        . '<a href="./index.php?pagina=1&cognoms=' . $_SESSION["cognoms"] . '"><img src="./assets/img/home.png" alt="inicio"/></a>'
+        . '<a href="./index.php?pag=atras&cognoms=' . $_SESSION["cognoms"] . '"><img src="./assets/img/left.png" alt="atrás"/></a>'
+        . '<a href="./index.php?pag=adelante&cognoms=' . $_SESSION["cognoms"] . '"><img src="./assets/img/right.png" alt="adelante"/></a>'
+        . '<a href="./index.php?pagina=' . $paginas . '&cognoms=' . $_SESSION["cognoms"] . '"><img src="./assets/img/end.png" alt="final"/></a>'
+        . '</td>';
 
-echo "<td class='menuAdd' colspan='3'><a href='./index.php?pagina=1'> <img src='./assets/img/add.png' alt='inicio' /></a></td>";
+} else if (isset($_GET["id"])) {
+    echo '<td class="menuMovimiento" colspan="3">'
+        . '<a href="./index.php?pagina=1&id=' . $_SESSION["id"] . '"><img src="./assets/img/home.png" alt="inicio"/></a>'
+        . '<a href="./index.php?pag=atras&id=' . $_SESSION["id"] . '"><img src="./assets/img/left.png" alt="atrás"/></a>'
+        . '<a href="./index.php?pag=adelante&id=' . $_SESSION["id"] . '"><img src="./assets/img/right.png" alt="adelante"/></a>'
+        . '<a href="./index.php?pagina=' . $paginas . '&id=' . $_SESSION["id"] . '"><img src="./assets/img/end.png" alt="final"/></a>'
+        . '</td>';
+} else {
+    echo '<td class="menuMovimiento" colspan="3">'
+        . '<a href="./index.php?pagina=1"><img src="./assets/img/home.png" alt="inicio"/></a>'
+        . '<a href="./index.php?pag=atras"><img src="./assets/img/left.png" alt="atrás"/></a>'
+        . '<a href="./index.php?pag=adelante"><img src="./assets/img/right.png" alt="adelante"/></a>'
+        . '<a href="./index.php?pagina=' . $paginas . '"><img src="./assets/img/end.png" alt="final"/></a>'
+        . '</td>';
+}
+
+echo "<td class='menuAdd' colspan='3'><a href='./add.php'> <img src='./assets/img/add.png' alt='inicio' /></a></td>";
 echo '</tr>';
 
 echo '</tbody>';
@@ -84,18 +191,30 @@ echo "</table>";
 echo "</div>";
 // Fin mostrar información
 
-
 echo '<div class="infoSesion">'
-    . 'Página: ' 
-    . $_SESSION["pagina"]
-    . '</br>'
-    . 'Página: ' 
-   // . $_SESSION["id"]
-    . '</br>'
-    . 'Página: ' 
-   // . $_SESSION["cognom"]
-    . '</br>'
-    .'</div> ';
+. 'Página: '
+. $_SESSION["pagina"]
+. '</br>'
+. 'Página: '
+// . $_SESSION["id"]
+ . '</br>'
+. 'Página: '
+// . $_SESSION["cognom"]
+ . '</br>'
+    . '</div> ';
+
+ /*// Si se desea destruir la sesión completamente, borre también la cookie de sesión.
+// Nota: ¡Esto destruirá la sesión, y no la información de la sesión!
+if (ini_get("session.use_cookies")) {
+$params = session_get_cookie_params();
+setcookie(session_name(), '', time() - 42000,
+$params["path"], $params["domain"],
+$params["secure"], $params["httponly"]
+);
+}
+
+// Finalmente, destruir la sesión.
+session_destroy();*/
 
 ?>
 
